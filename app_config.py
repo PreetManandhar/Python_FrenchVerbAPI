c48c3a233eaaff794a9f35ea92a137ef1215690f
PreetManandhar <145702104+PreetManandhar@users.noreply.github.com>
2024-02-09 17:38:36 -0500

    Add files via upload

---
+++ app_config.py
@@ -0,0 +1,6 @@
+CONST_MONGO_URL = "mongodb+srv://renancavalcanti:PQ7TJXmsdxosVqjP@cluster0.i9ldur9.mongodb.net/?retryWrites=true&w=majority"
+CONST_DATABASE_NAME = "TEST"
+CONST_USER_COLLECTION = "users"
+
+TOKEN_SECRET = "iosandpython"
+JWT_EXPIRATION = 86400*30
